4a8d4ec3f0d8c7e2d499e7c8360d3a047d82b31d
JackAlexanderClark <jackalexanderclark@protonmail.com>
2022-05-06 14:22:44 +0000

    1

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@
 
 // Using the keyword 'this' to illustrate that it's THIS clicked paragraph not all paragraphs
 $('button')mouseenter(function () {
-    $('this').removeClass('makeRed').addClass('makeBlue');
+    $(this).removeClass('makeRed').addClass('makeBlue');
 });
 
 $('button')mouseleave(function () {
-    $('this').removeClass('makeBlue').addClass('makeRed');
+    $(this).removeClass('makeBlue').addClass('makeRed');
 });
 
 
